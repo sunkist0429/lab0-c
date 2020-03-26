ef28126b5a62ea28f70bb543e5ed37752bc4c61a
sunkist <sunkist0429@gmail.com>
2020-03-26 01:23:28 +0000

    1. Fix the coding style 2. Fix some bugs

--- queue.c
+++ queue.c
@@ -9,10 +9,11 @@
  * Create empty queue.
  * Return NULL if could not allocate space.
  */
-queue_t* q_new()
+queue_t *q_new()
 {
-    queue_t* q = malloc(sizeof(queue_t));
-    if (q == NULL) return NULL;
+    queue_t *q = malloc(sizeof(queue_t));
+    if (q == NULL)
+        return NULL;
 
     q->head = NULL;
     q->tail = NULL;
@@ -22,14 +23,15 @@ queue_t* q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(queue_t* q)
+void q_free(queue_t *q)
 {
     /* TODO: How about freeing the list elements and the strings? */
-    if(q == NULL) return;
+    if (q == NULL)
+        return;
 
-    list_ele_t* traversal = q->head;
-    while (traversal != NULL){
-        list_ele_t* cur = traversal;
+    list_ele_t *traversal = q->head;
+    while (traversal != NULL) {
+        list_ele_t *cur = traversal;
         traversal = cur->next;
         free(cur->value);
         free(cur);
@@ -46,28 +48,30 @@ void q_free(queue_t* q)
  * Argument s points to the string to be stored.
  * The function must explicitly allocate space and copy the string into it.
  */
-bool q_insert_head(queue_t* q, char* s)
+bool q_insert_head(queue_t *q, char *s)
 {
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
-    if(q == NULL) return false;
+    if (q == NULL)
+        return false;
 
     // new element
-    list_ele_t* newh = malloc(sizeof(list_ele_t));
-    if (newh == NULL) return false;
-    newh->value = malloc(sizeof(char*));
-    if (newh->value == NULL) return false;
-    strcpy(newh->value, s);
-    newh->next = NULL;
+    list_ele_t *newh = malloc(sizeof(list_ele_t));
+    if (newh == NULL)
+        return false;
 
-    if (q->tail == NULL)    q->tail = newh;
-    if (q->head == NULL)
-    {
-        q->head = newh;
-    }else{
-        newh->next = q->head;
-        q->head = newh;
+    newh->value = malloc(strlen(s) + 1);
+    if (newh->value == NULL) {
+        free(newh);
+        return false;
     }
+    memcpy(newh->value, s, strlen(s) + 1);
+    newh->next = q->head;
+    q->head = newh;
+
+    if (q->tail == NULL)
+        q->tail = newh;
+
     q->size++;
 
     return true;
@@ -80,22 +84,31 @@ bool q_insert_head(queue_t* q, char* s)
  * Argument s points to the string to be stored.
  * The function must explicitly allocate space and copy the string into it.
  */
-bool q_insert_tail(queue_t* q, char* s)
+bool q_insert_tail(queue_t *q, char *s)
 {
-    if(q == NULL) return false;
+    if (q == NULL)
+        return false;
 
     // new element
-    list_ele_t* newh = malloc(sizeof(list_ele_t));
-    if (newh == NULL) return false;
-    newh->value = malloc(sizeof(char*));
-    if (newh->value == NULL) return false;
-    strcpy(newh->value, s);
+    list_ele_t *newh = malloc(sizeof(list_ele_t));
+    if (newh == NULL)
+        return false;
+    newh->value = malloc(strlen(s) + 1);
+    if (newh->value == NULL) {
+        free(newh);
+        return false;
+    }
+
+    memcpy(newh->value, s, strlen(s) + 1);
     newh->next = NULL;
 
-    if(q->head == NULL) q->head = newh;
-    if(q->tail == NULL) q->tail = newh;
-    else q->tail->next = newh;
-    q->tail = newh;
+    if (q->tail == NULL) {
+        q->tail = newh;
+        q->head = newh;
+    } else {
+        q->tail->next = newh;
+        q->tail = newh;
+    }
 
     q->size++;
 
@@ -110,17 +123,19 @@ bool q_insert_tail(queue_t* q, char* s)
  * (up to a maximum of bufsize-1 characters, plus a null terminator.)
  * The space used by the list element and the string should be freed.
  */
-bool q_remove_head(queue_t* q, char* sp, size_t bufsize)
+bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
 {
-    if (q == NULL)  return false;
-    if (q->size == 0) return false;
+    if (q == NULL)
+        return false;
+    if (q->size == 0)
+        return false;
 
-    if (sp != NULL)
-    {
-        strcpy(sp, q->head->value);
+    if (sp != NULL) {
+        memcpy(sp, q->head->value, bufsize);
+        sp[bufsize - 1] = '\0';
     }
 
-    list_ele_t* removeh = q->head;
+    list_ele_t *removeh = q->head;
     q->head = q->head->next;
     q->size--;
 
@@ -134,9 +149,10 @@ bool q_remove_head(queue_t* q, char* sp, size_t bufsize)
  * Return number of elements in queue.
  * Return 0 if q is NULL or empty
  */
-int q_size(queue_t* q)
+int q_size(queue_t *q)
 {
-    if(q == NULL) return 0;
+    if (q == NULL)
+        return 0;
 
     return q->size;
 }
@@ -148,35 +164,29 @@ int q_size(queue_t* q)
  * (e.g., by calling q_insert_head, q_insert_tail, or q_remove_head).
  * It should rearrange the existing ones.
  */
-void q_reverse(queue_t* q)
+void q_reverse(queue_t *q)
 {
-    if(q == NULL) return;
-    if(q->size < 2) return;
+    if (q == NULL)
+        return;
+    if (q->size < 2)
+        return;
 
-    list_ele_t *old_head = q -> head;
-    list_ele_t *old_tail = q -> tail;
-    list_ele_t *a = q -> head;
-    list_ele_t *b = q -> head -> next;
-    list_ele_t *c = q -> head -> next -> next;
+    list_ele_t *old_head = q->head;
+    list_ele_t *old_tail = q->tail;
+    list_ele_t *a = q->head;
+    list_ele_t *b = q->head->next;
+    list_ele_t *c = q->head->next->next;
 
-    b -> next = a;
-    a -> next = NULL;
-    while(c != NULL) {
+    b->next = a;
+    a->next = NULL;
+    while (c != NULL) {
         a = b;
         b = c;
-        c = c -> next;
-        b -> next = a;
+        c = c->next;
+        b->next = a;
     }
-    q -> head = old_tail;
-    q -> tail = old_head;
-}
-
-void swap(list_ele_t* a, list_ele_t* b)
-{
-    char* temp;
-    temp = a->value;
-    a->value = b->value;
-    b->value = temp;
+    q->head = old_tail;
+    q->tail = old_head;
 }
 
 /*
@@ -184,29 +194,53 @@ void swap(list_ele_t* a, list_ele_t* b)
  * No effect if q is NULL or empty. In addition, if q has only one
  * element, do nothing.
  */
-void q_sort(queue_t* q)
+list_ele_t *merge(list_ele_t *l1, list_ele_t *l2)
 {
-    if (q == NULL)  return;
+    // merge with recursive
+    if (!l2)
+        return l1;
+    if (!l1)
+        return l2;
 
-    /* Bubble sort the given linked list */
-    int swapped;
-    list_ele_t* ptr1;
-    list_ele_t* lptr = NULL;
-
-    do
-    {
-        swapped = 0;
-        ptr1 = q->head;
-        while (ptr1->next != lptr)
-        {
-            if (ptr1->value > ptr1->next->value)
-            {
-                swap(ptr1, ptr1->next);
-                swapped = 1;
-            }
-            ptr1 = ptr1->next;
-        }
-        lptr = ptr1;
+    if (strcmp(l1->value, l2->value) < 0) {
+        l1->next = merge(l1->next, l2);
+        return l1;
+    } else {
+        l2->next = merge(l1, l2->next);
+        return l2;
     }
-    while (swapped);
+}
+
+list_ele_t *mergeSortList(list_ele_t *head)
+{
+    // merge sort
+    if (!head || !head->next)
+        return head;
+
+    list_ele_t *fast = head->next;
+    list_ele_t *slow = head;
+
+    // split list
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    fast = slow->next;
+    slow->next = NULL;
+
+    // sort each list
+    list_ele_t *l1 = mergeSortList(head);
+    list_ele_t *l2 = mergeSortList(fast);
+
+    // merge sorted l1 and sorted l2
+    return merge(l1, l2);
+}
+
+void q_sort(queue_t *q)
+{
+    if (q == NULL)
+        return;
+
+    // merge sort
+    q->head = mergeSortList(q->head);
 }
--- queue.h
+++ queue.h
@@ -14,20 +14,18 @@
 /* Data structure declarations */
 
 /* Linked list element (You shouldn't need to change this) */
-typedef struct ELE
-{
+typedef struct ELE {
     /* Pointer to array holding string.
      * This array needs to be explicitly allocated and freed
      */
-    char* value;
-    struct ELE* next;
+    char *value;
+    struct ELE *next;
 } list_ele_t;
 
 /* Queue structure */
-typedef struct
-{
-    list_ele_t* head; /* Linked list of elements */
-    list_ele_t* tail; /* Linked to last elements */
+typedef struct {
+    list_ele_t *head; /* Linked list of elements */
+    list_ele_t *tail; /* Linked to last elements */
     int size;
 } queue_t;
 
@@ -37,13 +35,13 @@ typedef struct
  * Create empty queue.
  * Return NULL if could not allocate space.
  */
-queue_t* q_new();
+queue_t *q_new();
 
 /*
  * Free ALL storage used by queue.
  * No effect if q is NULL
  */
-void q_free(queue_t* q);
+void q_free(queue_t *q);
 
 /*
  * Attempt to insert element at head of queue.
@@ -52,7 +50,7 @@ void q_free(queue_t* q);
  * Argument s points to the string to be stored.
  * The function must explicitly allocate space and copy the string into it.
  */
-bool q_insert_head(queue_t* q, char* s);
+bool q_insert_head(queue_t *q, char *s);
 
 /*
  * Attempt to insert element at tail of queue.
@@ -61,7 +59,7 @@ bool q_insert_head(queue_t* q, char* s);
  * Argument s points to the string to be stored.
  * The function must explicitly allocate space and copy the string into it.
  */
-bool q_insert_tail(queue_t* q, char* s);
+bool q_insert_tail(queue_t *q, char *s);
 
 /*
  * Attempt to remove element from head of queue.
@@ -71,13 +69,13 @@ bool q_insert_tail(queue_t* q, char* s);
  * (up to a maximum of bufsize-1 characters, plus a null terminator.)
  * The space used by the list element and the string should be freed.
  */
-bool q_remove_head(queue_t* q, char* sp, size_t bufsize);
+bool q_remove_head(queue_t *q, char *sp, size_t bufsize);
 
 /*
  * Return number of elements in queue.
  * Return 0 if q is NULL or empty
  */
-int q_size(queue_t* q);
+int q_size(queue_t *q);
 
 /*
  * Reverse elements in queue
@@ -86,13 +84,15 @@ int q_size(queue_t* q);
  * (e.g., by calling q_insert_head, q_insert_tail, or q_remove_head).
  * It should rearrange the existing ones.
  */
-void q_reverse(queue_t* q);
+void q_reverse(queue_t *q);
 
 /*
  * Sort elements of queue in ascending order
  * No effect if q is NULL or empty. In addition, if q has only one
  * element, do nothing.
  */
-void q_sort(queue_t* q);
+list_ele_t *merge(list_ele_t *l1, list_ele_t *l2);
+list_ele_t *mergeSortList(list_ele_t *head_ref);
+void q_sort(queue_t *q);
 
 #endif /* LAB0_QUEUE_H */
